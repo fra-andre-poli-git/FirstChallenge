f6ebab9e2e2474383b84c7b9af334ec1c6c70001
Francesco Andreotti <francesco.andreotti@mail.polimi.it>
2025-03-19 17:11:59 +0100

    Added elements to the struct and normSquared declaration

--- Function.hpp
+++ Function.hpp
@@ -1,16 +1,23 @@
 #ifndef FUNCTION_HEADER_FILE
 #define FUNCTION_HEADER_FILE
 #include<functional>
+#include<vector>
 
 struct parameters{
   Point initialPoint;
   double stepTol;
   double resTol;
   double initialLearningRate;
+  double sigma;
+  double minAlpha;
   unsigned int maxIt;
 };
 
-returnType findMinimum(std::function<double(Point)>, gradf,
+Point findMinimum(std::function<double(Point)> f,
+  std::function<std::vector<double>(Point)> gradf,
   const parameters & params);
 
+// This is the declaration of the function that returns the squared norm of a
+// vector
+double normSquared(const std::vector<double> vec);
 #endif
